6f047dd9bcf0dd5c7b22787496e3fa997f99130e
anoorug <dreamgaming281@gmail.com>
2025-12-24 16:32:39 +0545

    navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,69 @@
+import ads_icon from './ads_icon.svg'
+import airbnb_logo from './airbnb_logo.svg'
+import arrow_icon from './arrow_icon.svg'
+import bgImage1 from './bgImage1.png'
+import bgImage2 from './bgImage2.png'
+import close_icon from './close_icon.svg'
+import content_icon from './content_icon.svg'
+import coinbase_logo from './coinbase_logo.png'
+import email_icon from './email_icon.svg'
+import facebook_icon from './facebook_icon.svg'
+import google_logo from './google_logo.svg'
+import group_profile from './group_profile.png'
+import hero_img from './hero_img.png'
+import instagram_icon from './instagram_icon.svg'
+import linkedin_icon from './linkedin_icon.svg'
+import logo_dark from './logo_dark.svg'
+import logo from './logo.svg'
+import marketing_icon from './marketing_icon.svg'
+import menu_icon_dark from './menu_icon_dark.svg'
+import menu_icon from './menu_icon.svg'
+import microsoft_logo from './microsoft_logo.png'
+import moon_icon from './moon_icon.svg'
+import person_icon from './person_icon.svg'
+import rakuten_logo from './rakuten_logo.png'
+import social_icon from './social_icon.svg'
+import sun_icon from './sun_icon.svg'
+import twitter_icon from './twitter_icon.svg'
+import work_dashboard from './work_dashboard_management.png'
+import work_fitness_app from './work_fitness_app.png'
+import work_mobile_app from './work_mobile_app.png'
+import zoom_logo from './zoom_logo.png'
+
+
+export const company_logos = [
+    airbnb_logo,
+    zoom_logo,
+    rakuten_logo,
+    coinbase_logo,
+    microsoft_logo,
+    google_logo
+] 
+const assets ={
+    ads_icon,
+    arrow_icon,
+    bgImage1,
+    bgImage2,
+    close_icon,
+    content_icon,
+    email_icon,
+    facebook_icon,
+    group_profile,
+    hero_img,
+    instagram_icon,
+    linkedin_icon,
+    logo,
+    logo_dark,
+    marketing_icon,
+    menu_icon,
+    menu_icon_dark,
+    moon_icon,
+    person_icon,
+    social_icon,
+    sun_icon,
+    twitter_icon,
+    work_dashboard,
+    work_fitness_app,
+    work_mobile_app,
+}
+export default assets
